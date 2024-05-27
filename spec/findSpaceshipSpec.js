c516e4081c3707cf236b69238a845c4c784b5287
thomas baumann <thomasbaumann@gmx.at>
2024-05-27 10:36:20 +0200

    refactored function to actually check map(working), changed order of to do list

--- spec/findSpaceshipSpec.js
+++ spec/findSpaceshipSpec.js
@@ -4,17 +4,20 @@ describe('Find Spaceship', function() {
 	});
 
 	it('should return "Spaceship lost forever" for an empty string', function() {
-		expect(spaceship.findSpaceship()).toBe("Spaceship lost forever");
+		const map = "";
+		expect(spaceship.findSpaceship(map)).toBe("Spaceship lost forever");
 	});
 });
 
 /*
 ToDoList:
 const map="" -> "Spaceship lost forever"
+const map="..........\n..........\n" -> "Map incomplete"
+const map=".X........\n..........\n..........\n..........\n..........\nX........." -> "Map faulty"
 const map="..........\n..........\n..........\n..........\n..........\n.........." -> "Spaceship lost forever"
 const map="X.........\n..........\n..........\n..........\n..........\n.........." -> "[0,5]"
 const map="..........\n..........\n..........\n..........\n..........\nX........." -> "[0,0]"
-const map=".X........\n..........\n..........\n..........\n..........\nX........." -> "Map faulty"
-const map="..........\n..........\n" -> "Map incomplete"
+
+
 
 */
--- src/findSpaceship.js
+++ src/findSpaceship.js
@@ -1,5 +1,15 @@
 // StringCalculator.js
-function Spaceship() {}
-Spaceship.prototype.findSpaceship = function(map) {
+function Spaceship() { }
+Spaceship.prototype.findSpaceship = function (map) {
+	const rows = map.split('\n');
+
+	for (let y = 0; y < rows.length; y++) {
+		for (let x = 0; x < rows[y].length; x++) {
+			if (rows[y][x] === 'X') {
+				return [x, rows.length - y - 1];
+			}
+		}
+	}
+
 	return "Spaceship lost forever";
 };
