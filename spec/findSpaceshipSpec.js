9917eb3a4f6959995488408c1aad5ee9e485a4bc
thomas baumann <thomasbaumann@gmx.at>
2024-05-27 10:40:28 +0200

    added failing test step 2 (failing)

--- spec/findSpaceshipSpec.js
+++ spec/findSpaceshipSpec.js
@@ -7,17 +7,19 @@ describe('Find Spaceship', function() {
 		const map = "";
 		expect(spaceship.findSpaceship(map)).toBe("Spaceship lost forever");
 	});
+	it('should return "Map incomplete" if the map is incomplete', function() {
+		const map = "..........\n..........\n";
+		expect(spaceship.findSpaceship(map)).toBe("Map incomplete");
+	});
 });
 
 /*
 ToDoList:
-const map="" -> "Spaceship lost forever"
-const map="..........\n..........\n" -> "Map incomplete"
-const map=".X........\n..........\n..........\n..........\n..........\nX........." -> "Map faulty"
-const map="..........\n..........\n..........\n..........\n..........\n.........." -> "Spaceship lost forever"
-const map="X.........\n..........\n..........\n..........\n..........\n.........." -> "[0,5]"
-const map="..........\n..........\n..........\n..........\n..........\nX........." -> "[0,0]"
-
-
+1. const map="" -> "Spaceship lost forever"
+2. const map="..........\n..........\n" -> "Map incomplete"
+3. const map=".X........\n..........\n..........\n..........\n..........\nX........." -> "Map faulty"
+4. const map="..........\n..........\n..........\n..........\n..........\n.........." -> "Spaceship lost forever"
+5. const map="X.........\n..........\n..........\n..........\n..........\n.........." -> "[0,5]"
+6. const map="..........\n..........\n..........\n..........\n..........\nX........." -> "[0,0]"
 
 */
